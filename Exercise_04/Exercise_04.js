c427f43ab757602eceb350493d5e989b8858c947
Aeesh <adamaisha352@gmail.com>
2020-09-21 23:04:22 +0100

    initial commit

--- Exercise_04/Exercise_04.js
+++ Exercise_04/Exercise_04.js
@@ -28,17 +28,17 @@ display = '';
 for(let detail of details){
     display += `
     <div> 
-    <img src="${detail.logo}" alt="" style="height:100px"> <br>
-    <strong> Name </strong>: ${detail.name} <br>
-    <strong> Email </strong>: ${detail.email} <br>
-    <strong> Phone </strong>: ${detail.phone} <br>
-    <strong> Location </strong>: ${detail.location} <br>
-    <button onclick="del()" style="background:red">Delete</button>
-    <button onclick="edit()" style="background:yellow">Edit</button>
-    </div> <hr>
-    `
+        <img src="${detail.logo}" alt="" style="height:100px"> <br>
+        <strong> Name </strong>: ${detail.name} <br>
+        <strong> Email </strong>: ${detail.email} <br>
+        <strong> Phone </strong>: ${detail.phone} <br>
+        <strong> Location </strong>: ${detail.location} <br>
+        <button onclick="del(${details.indexOf(detail)})" style="background:red">Delete</button>
+        <button onclick="edit()" style="background:yellow">Edit</button>
+    </div> `
 }
-/* FUNCTION TO ADD DATA  */
+document.getElementById("data").innerHTML = display;
+    // FUNCTION TO ADD DATA 
  add = () => {
     newDetails={
     "name" : prompt('Input Company\'s Name'),
@@ -50,29 +50,37 @@ for(let detail of details){
     
     // APPENDS OBJECT 'addDetails' TO ARRAY 'newDetails'
     details.push(newDetails);
-    for(i=details.length-1; i<details.length; i++) {
+    for( i= details.length-1; i < details.length; i++){
         display += `
         <div> 
-        <img src="${details[i].logo}" alt="" style="height:100px"> <br>
-        <strong> Name </strong>: ${details[i].name} <br>
-        <strong> Email </strong>: ${details[i].email} <br>
-        <strong> Phone </strong>: ${details[i].phone} <br>
-        <strong> Location </strong>: ${details[i].location} <br>
-        <button onclick="del()" style="background:red">Delete</button>
-        <button onclick="edit()" style="background:yellow">Edit</button>
-        </div> <hr>
-        `
+            <img src="${details[i].logo}" alt="" style="height:100px"> <br>
+            <strong> Name </strong>: ${details[i].name} <br>
+            <strong> Email </strong>: ${details[i].email} <br>
+            <strong> Phone </strong>: ${details[i].phone} <br>
+            <strong> Location </strong>: ${details[i].location} <br>
+            <button onclick="del(${i})" style="background:red">Delete</button>
+            <button onclick="edit()" style="background:yellow">Edit</button>
+        </div>   `
     }
     document.getElementById("data").innerHTML = display;
 }
 
-/* FUNCTION TO DELETE DATA  */
-del = () => {
-    details.slice(0,1)
-    for(let detail of details) {
-        details.splice(details[detail],1);
+    // FUNCTION TO DELETE DATA 
+del = (x) => {
+    details.splice(x,1)
+    display = ""
+    for(let detail of details){
+        display += `
+        <div> 
+            <img src="${detail.logo}" alt="" style="height:100px"> <br>
+            <strong> Name </strong>: ${detail.name} <br>
+            <strong> Email </strong>: ${detail.email} <br>
+            <strong> Phone </strong>: ${detail.phone} <br>
+            <strong> Location </strong>: ${detail.location} <br>
+            <button onclick="del(${details.indexOf(detail)})" style="background:red">Delete</button>
+            <button onclick="edit()" style="background:yellow">Edit</button>
+        </div> 
+        `
     }
+    document.getElementById("data").innerHTML = display;
 }
-
-/* ASSIGNING VARIABLE TO DISPLAY IN DOM  */
-document.getElementById("data").innerHTML = display;
